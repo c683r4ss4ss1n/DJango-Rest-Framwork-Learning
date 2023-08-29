8739deb44c5924dd74412464f6d1a46ef9b88597
R Rafiou Khan Eshan <140967916+c683r4ss4ss1n@users.noreply.github.com>
2023-08-29 16:54:31 +0600

    User Registration without Serializer using Django Rest Framework

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,5 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.shortcuts import render
+
+
+
+def homeView(request):
+    return render(request, 'index.html')
 
 @api_view(['GET', 'POST'])
 def firstAPI(request):
@@ -14,3 +20,33 @@ def firstAPI(request):
         'University' : 'DIU'
     }
     return Response(context)
+
+
+from django.contrib.auth.models import User
+
+@api_view(['POST'])
+def registrationAPI(request):
+    if request.method == 'POST':
+        username = request.data['username']
+        email = request.data['email']
+        first_name = request.data['first_name']
+        last_name = request.data['last_name']
+        password1 = request.data['password1']
+        password2 = request.data['password2']
+
+        if User.objects.filter(username = username).exists():
+            return Response({'error': 'An user with this username already exists!'})
+        if password1 != password2:
+            return Response({'error':'password didnot match'})
+        
+        user = User()
+        user.username =username
+        user.email = email
+        user.first_name = first_name
+        user.last_name = last_name
+        user.is_active = True
+
+        user.set_password(raw_password = password1)
+        user.save()
+
+        return Response({'Success' : 'User successfully registered'})
