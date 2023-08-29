af71d0a5c79f0bb0105287fb58dafe618735678e
R Rafiou Khan Eshan <140967916+c683r4ss4ss1n@users.noreply.github.com>
2023-08-29 17:21:46 +0600

    Token Based Login API in Django

--- myproject/urls.py
+++ myproject/urls.py
@@ -3,13 +3,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from firstapp.views import homeView
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('rest_framework.urls')),
     path('api/firstapp/', include('firstapp.urls')),
-    path('', homeView)
+    path('', homeView),
+    path('api/login-api/', obtain_auth_token),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
